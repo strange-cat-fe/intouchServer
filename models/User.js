8ec43264c1c34ea5b1d5294c8ca4e780574e292b
Danila Osin <danilaosin12@gmail.com>
2020-09-04 15:01:04 +0300

    create user model

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require('mongoose')
+
+const user = new Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = model('User', user)
