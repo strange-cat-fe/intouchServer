a1e833e83961a08f15ce9bb0f47e5e11eb67eb57
Danila Osin <danilaosin12@gmail.com>
2020-09-12 23:20:56 +0300

    change db config export

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const app = require('./app')
-const connectMongoose = require('./lib/mongoose-connect')
+const mongoDB = require('./lib/mongoose-connect')
 
-connectMongoose().then((message) => {
+mongoDB.connect().then((message) => {
   console.log(message)
   app.listen(5000, () => console.log('Server is running on port 5000...'))
 })
