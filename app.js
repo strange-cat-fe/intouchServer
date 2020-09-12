28edc84eddbe67cc8e55a94d4c771502813d69e8
Danila Osin <danilaosin12@gmail.com>
2020-09-12 14:33:54 +0300

    add test db and main db connection

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const app = require('express')()
 
-async function start() {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://danila:19283746@cluster0-jati1.mongodb.net/intouchServer',
-      {
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+const jsonParser = require('body-parser').json()
 
-    app.listen(5000, () => console.log('Server is running on port 5000...'))
+let DB_URI = process.env.DB_URI
+if (process.env.NODE_ENV === 'test') {
+  app.use(require('cors')())
+  DB_URI = 'mongodb://127.0.0.1:27017/intouchServer'
+  console.log('__Test is running on local db__')
+}
+
+app.use(jsonParser)
+app.use(require('./middleware/decodeToken.js'))
+
+app.use('/api/auth', require('./routes/auth.routes'))
+;(async function () {
+  try {
+    await mongoose.connect(DB_URI, {
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
   } catch (e) {
     console.log(e)
   }
-}
+})()
 
-start()
+module.exports = app.listen(5000, () =>
+  console.log('Server is running on port 5000...')
+)
