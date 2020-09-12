ad5ac554f944914d0290d9d75b820967ea4f5e4f
Danila Osin <danilaosin12@gmail.com>
2020-09-12 15:53:53 +0300

    add token verification

--- middleware/decodeToken.js
+++ middleware/decodeToken.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
 module.exports = function (req, res, next) {
   try {
-    if (req.originalUrl.includes('/api/auth')) return next()
+    //if (req.originalUrl.includes('/api/auth')) return next()
 
     const encodedToken = req.header('AUTHORIZATION').split(' ')[1]
-    const decodedToken = jwt.verify(encodedToken, process.env.JWT_SECRET)
-    res.user = decodedToken
+    const { _id } = jwt.verify(encodedToken, process.env.JWT_SECRET)
+
+    User.findById(_id).then((user) => {
+      if (user) res.user = user
+      else return res.status(401)
+    })
 
     next()
-  } catch (e) {}
+  } catch (e) {
+    res.status(200).json({ error: e.message })
+  }
 }
