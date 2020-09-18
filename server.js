e398f96430333bf0df33142ff4ada6a74341afd9
Danila Osin <danilaosin12@gmail.com>
2020-09-18 11:37:06 +0300

    deploy 2

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const mongoDB = require('./lib/mongoose-connect')
 
 mongoDB.connect().then((message) => {
   console.log(message)
-  app.listen(5000, () => console.log('Server is running on port 5000...'))
+  app.listen(process.env.PORT || 5000, () =>
+    console.log('Server is running on port 5000...')
+  )
 })
