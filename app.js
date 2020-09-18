f6a93a8ff59357cdcd05d90aa7261ac78d6525e7
Danila Osin <danilaosin12@gmail.com>
2020-09-18 19:08:21 +0300

    enable cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const middlewares = require('./middleware/index')
 const routes = require('./routes/index')
 
-process.env.NODE_ENV == 'test' && app.use(cors())
+app.use(cors())
 
 middlewares.forEach((m) => app.use(m))
 routes.forEach((r) => app.use(r.prefix, r.route))
